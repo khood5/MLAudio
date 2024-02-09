d53d97fdc8307bfacf4d546e7a81ef7ffea324a8
Kendric Hood <khood5@kent.edu>
2024-02-09 06:26:36 +0000

    swaped threading for multiprocessing

--- data/makeBackgroundAudio.py
+++ data/makeBackgroundAudio.py
@@ -7,6 +7,7 @@ import csv
 from tqdm import tqdm
 import threading
 import uuid
+from multiprocessing import Process, Manager
 
 def makeBackgroundAudio(audioBits, outputPath, length=60):
     BackgroundAudio = AudioSegment.empty()
@@ -26,7 +27,7 @@ def main():
     parser.add_argument('indexfile_path', type=str, help='Path to the index file (csv file with list of paths to background sound files)')
     parser.add_argument('number_of_samples_to_make', type=int, help='Number of samples to generate')
     parser.add_argument('-l', '--length', nargs='?', default=60, type=int, help='length of each sample default=60')
-    parser.add_argument('-t', '--num_threads', nargs='?', default=2, type=int, help='number of threads to use default=2')
+    parser.add_argument('-p', '--num_processes', nargs='?', default=2, type=int, help='number of processes to use default=2')
     parser.add_argument('output_directory', type=str, help='Path to the output directory')
     args = parser.parse_args()
     
@@ -43,22 +44,38 @@ def main():
     print(f"Putting samples in {args.output_directory}")
     remainingSamples = args.number_of_samples_to_make
     outputFileNames = []
+
     with tqdm(total=args.number_of_samples_to_make) as pbar:
-        while remainingSamples > 0: 
-        # in tqdm(range(args.number_of_samples_to_make)):
-            num_threads = args.num_threads if (remainingSamples - args.num_threads) > 0 else remainingSamples
-            threads = []
-            for i in range(num_threads):
-                # use uuid.uuid4() to make a random UUID so file names dont collided 
-                outputFileNames.append(f"{args.output_directory}/background_{uuid.uuid4()}.wav")
-                thread = threading.Thread(target=makeBackgroundAudio, args=(backgoundSoundBytes, outputFileNames[-1], args.length,))
-                threads.append(thread)
-                thread.start()
-            # makeBackgroundAudio(backgoundSoundBytes, args.length).export(f"{args.output_directory}/background{i}.wav", format="wav", codec="pcm_s32le")
-            for thread in threads:
-                thread.join()
-            pbar.update(num_threads)
-            remainingSamples = remainingSamples - num_threads
+        remainingSamples = args.number_of_samples_to_make
+        while remainingSamples > 0:
+            numProcesses = args.num_processes if (remainingSamples - args.num_processes) > 0 else remainingSamples
+            processList = []
+            for _ in range(numProcesses):
+                    # use uuid.uuid4() to make a random UUID so file names dont collided 
+                    outputFileNames.append(f"{args.output_directory}/background_{uuid.uuid4()}.wav")
+                    process = Process(target=makeBackgroundAudio, args=(backgoundSoundBytes, outputFileNames[-1], args.length,))
+                    processList.append(process)
+                    process.start()
+            for process in processList:
+                process.join()
+            pbar.update(numProcesses)
+            remainingSamples = remainingSamples - numProcesses
+    # with tqdm(total=args.number_of_samples_to_make) as pbar:
+    #     while remainingSamples > 0: 
+    #     # in tqdm(range(args.number_of_samples_to_make)):
+    #         num_threads = args.num_threads if (remainingSamples - args.num_threads) > 0 else remainingSamples
+    #         threads = []
+    #         for i in range(num_threads):
+    #             # use uuid.uuid4() to make a random UUID so file names dont collided 
+    #             outputFileNames.append(f"{args.output_directory}/background_{uuid.uuid4()}.wav")
+    #             thread = threading.Thread(target=makeBackgroundAudio, args=(backgoundSoundBytes, outputFileNames[-1], args.length,))
+    #             threads.append(thread)
+    #             thread.start()
+    #         # makeBackgroundAudio(backgoundSoundBytes, args.length).export(f"{args.output_directory}/background{i}.wav", format="wav", codec="pcm_s32le")
+    #         for thread in threads:
+    #             thread.join()
+    #         pbar.update(num_threads)
+    #         remainingSamples = remainingSamples - num_threads
     file = open('backgroundIndex.csv', 'w+', newline ='')
     with file:    
         write = csv.writer(file)
