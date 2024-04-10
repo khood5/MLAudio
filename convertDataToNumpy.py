478591abbd9a60877632a84e19f4069e7b897a53
Kendric Hood <khood5@kent.edu>
2024-04-09 21:48:19 -0400

    added labels to convertDataToNumpy

--- convertDataToNumpy.py
+++ convertDataToNumpy.py
@@ -12,9 +12,9 @@ parser = argparse.ArgumentParser(description='Process train, valid, and test dat
 parser.add_argument('train_dataset', type=str, help='Path to the train dataset file')
 parser.add_argument('valid_dataset', type=str, help='Path to the valid dataset file')
 parser.add_argument('test_dataset', type=str, help='Path to the test dataset file')
-parser.add_argument('--train_out', default='neuroTrain.npy', type=str, help='Path to the train output file')
-parser.add_argument('--valid_out', default='neuroValid.npy', type=str, help='Path to the valid output file')
-parser.add_argument('--test_out', default='neuroTest.npy', type=str, help='Path to the test output file')
+parser.add_argument('--train_out', default='neuroTrain', type=str, help='Path to the train output file')
+parser.add_argument('--valid_out', default='neuroValid', type=str, help='Path to the valid output file')
+parser.add_argument('--test_out', default='neuroTest', type=str, help='Path to the test output file')
 args = parser.parse_args()
 
 train_dataset = args.train_dataset
@@ -40,23 +40,38 @@ timestep_dim = len(train_data[0][0][0][0])
 print("making training numpy file")
 data_instance_dim = len(train_data)
 neuroTrain = torch.zeros((data_instance_dim, features_dim, timestep_dim))
+neuroTrainLabels = torch.zeros((data_instance_dim))
 for i in tqdm(range(data_instance_dim)):
     neuroTrain[i] = train_data[i][0]
+    neuroTrainLabels[i] = train_data[i][1]
 neuroTrain = neuroTrain.numpy()
 np.save(args.train_out, neuroTrain)
+np.save(f"{args.train_out}_labels", neuroTrainLabels)
+
+neuroTrain = torch.zeros((data_instance_dim))
+for i in tqdm(range(data_instance_dim)):
+    neuroTrain[i] = train_data[i][1]
+neuroTrain = neuroTrain.numpy()
+
 
 print("making validation numpy file")
 data_instance_dim = len(valid_data)
 neuroValid = torch.zeros((data_instance_dim, features_dim, timestep_dim))
+neuroValidLabels = torch.zeros((data_instance_dim))
 for i in tqdm(range(data_instance_dim)):
     neuroValid[i] = valid_data[i][0]
+    neuroValidLabels[i] = valid_data[i][1]
 neuroValid = neuroValid.numpy()
 np.save(args.valid_out, neuroValid)
+np.save(f"{args.valid_out}_labels", neuroValidLabels)
 
 print("Making testing numpy file")
 data_instance_dim = len(test_data)
 neuroTest = torch.zeros((data_instance_dim, features_dim, timestep_dim))
+neuroTestLabels = torch.zeros((data_instance_dim))
 for i in tqdm(range(data_instance_dim)):
     neuroTest[i] = test_data[i][0]
+    neuroTestLabels[i] = test_data[i][1]
 neuroTest = neuroTest.numpy()
 np.save(args.test_out, neuroTest)
+np.save(f"{args.test_out}_labels", neuroTestLabels)
