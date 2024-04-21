7b1537f5cc90abac6315c6331117cf4e17ac5281
Kendric Hood <khood5@kent.edu>
2024-04-21 18:38:22 -0400

    made pip install a script

---
+++ pip_requirements.sh
@@ -0,0 +1,7 @@
+# important ones 
+pip install torch torchaudio torchvision soundfile tqdm matplotlib seaborn pandas uuid scikit-learn scipy
+pip install git+https://bitbucket.org/neuromorphic-utk/framework.git
+# test EONS install 
+python -c "import neuro; print(neuro.__doc__)"
+python -c "import eons; print(eons.__doc__)"
+python -c "import py_apps; print(py_apps.__doc__)"
