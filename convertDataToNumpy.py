69311fe99d2875c1b3012e5acd2b649e4557c4e9
Kendric Hood <khood5@kent.edu>
2024-04-21 22:39:49 +0000

    updated program to convert folders to numpy to not force a valid and test set

--- convertDataToNumpy.py
+++ convertDataToNumpy.py
@@ -8,8 +8,8 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Process train, valid, and test datasets.')
 parser.add_argument('train_dataset', type=str, help='Path to the train dataset file')
-parser.add_argument('valid_dataset', type=str, help='Path to the valid dataset file')
-parser.add_argument('test_dataset', type=str, help='Path to the test dataset file')
+parser.add_argument('-v', '--valid_dataset', default='', type=str, help='Path to the valid dataset file')
+parser.add_argument('-t', '--test_dataset', default='', type=str, help='Path to the test dataset file')
 parser.add_argument('--train_out', default='neuroTrain', type=str, help='Path to the train output file')
 parser.add_argument('--valid_out', default='neuroValid', type=str, help='Path to the valid output file')
 parser.add_argument('--test_out', default='neuroTest', type=str, help='Path to the test output file')
@@ -28,8 +28,6 @@ data_transform = transforms.Compose([
     ])
 
 train_data = audioDataloader(index_file=train_dataset, transforms=data_transform)
-valid_data = audioDataloader(index_file=valid_dataset, transforms=data_transform)
-test_data = audioDataloader(index_file=test_dataset, transforms=data_transform)
 
 timestep_dim = len(train_data[0][0][0][0])
 features_dim = len(train_data[0][0][0])
@@ -47,25 +45,28 @@ neuroTrain = neuroTrain.numpy()
 np.save(args.train_out, neuroTrain)
 np.save(f"{args.train_out}_labels", neuroTrainLabels)
 
+if(valid_dataset != ''):
+    valid_data = audioDataloader(index_file=valid_dataset, transforms=data_transform)
+    print("making validation numpy file")
+    data_instance_dim = len(valid_data)
+    print(f"Number of data instances: {data_instance_dim}")
+    neuroValid = torch.zeros((data_instance_dim, features_dim, timestep_dim))
+    neuroValidLabels = torch.zeros((data_instance_dim))
+    for i in tqdm(range(data_instance_dim)):
+        neuroValidLabels[i] = valid_data[i][1]
+    neuroValid = neuroValid.numpy()
+    np.save(args.valid_out, neuroValid)
+    np.save(f"{args.valid_out}_labels", neuroValidLabels)
 
-print("making validation numpy file")
-data_instance_dim = len(valid_data)
-print(f"Number of data instances: {data_instance_dim}")
-neuroValid = torch.zeros((data_instance_dim, features_dim, timestep_dim))
-neuroValidLabels = torch.zeros((data_instance_dim))
-for i in tqdm(range(data_instance_dim)):
-    neuroValidLabels[i] = valid_data[i][1]
-neuroValid = neuroValid.numpy()
-np.save(args.valid_out, neuroValid)
-np.save(f"{args.valid_out}_labels", neuroValidLabels)
-
-print("Making testing numpy file")
-data_instance_dim = len(test_data)
-print(f"Number of data instances: {data_instance_dim}")
-neuroTest = torch.zeros((data_instance_dim, features_dim, timestep_dim))
-neuroTestLabels = torch.zeros((data_instance_dim))
-for i in tqdm(range(data_instance_dim)):
-    neuroTestLabels[i] = test_data[i][1]
-neuroTest = neuroTest.numpy()
-np.save(args.test_out, neuroTest)
-np.save(f"{args.test_out}_labels", neuroTestLabels)
+if(test_dataset != ''):
+    test_data = audioDataloader(index_file=test_dataset, transforms=data_transform)
+    print("Making testing numpy file")
+    data_instance_dim = len(test_data)
+    print(f"Number of data instances: {data_instance_dim}")
+    neuroTest = torch.zeros((data_instance_dim, features_dim, timestep_dim))
+    neuroTestLabels = torch.zeros((data_instance_dim))
+    for i in tqdm(range(data_instance_dim)):
+        neuroTestLabels[i] = test_data[i][1]
+    neuroTest = neuroTest.numpy()
+    np.save(args.test_out, neuroTest)
+    np.save(f"{args.test_out}_labels", neuroTestLabels)
