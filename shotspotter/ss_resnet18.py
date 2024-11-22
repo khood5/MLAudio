b6884c0da5fe739b9192ea264035f284110b2908
Joao Donaton <joao.vitor.donaton@gmail.com>
2024-11-21 23:46:53 -0500

    analysis on audio samples
    TODO: try windows other than 50ms for computing decibels, look into more detail of average and peak plots to try to identify outliers

--- shotspotter/ss_resnet18.py
+++ shotspotter/ss_resnet18.py
@@ -4,7 +4,7 @@ from torch.nn import Linear
 from torchvision import models
 import numpy as np
 from torch.utils.data import DataLoader
-from torch.optim import SGD
+from torch.optim import SGD, Adam
 
 from ss_dataset import MosaicDataset
 
@@ -44,7 +44,8 @@ validation_loader = DataLoader(validation_set, batch_size=BATCH_SIZE, shuffle=Tr
 
 # Train
 loss_func = nn.CrossEntropyLoss()
-optimizer = SGD(model.parameters(), lr=0.1)
+#optimizer = SGD(model.parameters(), lr=0.1)
+optimizer = Adam(model.parameters(), lr=0.01, )
 
 for epoch in range(20):
     print(f'Training with {len(training_set)} training samples')
@@ -53,10 +54,11 @@ for epoch in range(20):
     # run accuracy on validation
     # TODO: breakdown by class accuracy
     total, correct = 0, 0
+    correct_with_gunshot = 0
     with torch.no_grad():
         for data in validation_loader:
             mosaics, labels = data
-                
+
             if torch.cuda.is_available():
                 mosaics = mosaics.to('cuda')
                 labels = labels.to('cuda')
@@ -64,13 +66,20 @@ for epoch in range(20):
             outputs = model(mosaics)
             v, ind = torch.max(outputs, 1)
 
+            # check what we got correct, then use logical_and to get the ones that are correct and of class 1 
+            correct_with_gunshot += torch.sum(torch.logical_and(ind == labels, labels))
+            print(correct_with_gunshot)
+
             correct += torch.sum(ind == labels).item()
             total += BATCH_SIZE
 
 
     print(f'Total is {total} and correct is {correct}')
+    print(f'correct in class 1 (with gunshot) {correct_with_gunshot}')
+    print(f'correct in class 0 (NO gunshot) {correct-correct_with_gunshot}')
     print(f'Accuracy is {correct / total}')
 
+    # compute and update gradients
     count = 0
     loss_sum = 0
     for mosaics, labels in training_loader:
