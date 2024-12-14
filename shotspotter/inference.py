c2e6e77715642df8eccfa87f9a3f019a26026092
Joao Donaton <joao.vitor.donaton@gmail.com>
2024-12-14 18:01:28 -0500

    inference time

--- shotspotter/inference.py
+++ shotspotter/inference.py
@@ -2,6 +2,7 @@ import argparse
 import torch
 from ss_dataset import MosaicDataset
 from torch.utils.data import DataLoader
+import time
 
 from common import RGB_MEAN, RGB_STD, DEVICE, model_18, model_152
 
@@ -20,8 +21,12 @@ if args.type == '18':
 else:
     model = model_152
 
-model.load_state_dict(torch.load(args.model_path, weights_only=True))
+# map from cuda to cpu (since we saved from cuda state) to infer on raspberry pi
+model.load_state_dict(torch.load(args.model_path, weights_only=True, map_location=torch.device('cpu')))
 model = model.to(DEVICE)
+model.eval() # disables batch norm
+
+t = time.time()
 
 correct = 0
 total = 0
@@ -38,4 +43,7 @@ with torch.no_grad():
         correct += torch.sum(ind == labels).item()
         total += BATCH_SIZE
 
-print(f'accuracy: {100 * correct / total} %')
+t1 = time.time()
+
+print(f'Accuracy on test set: {100 * correct / total} %')
+print(f'Total inference time: {t1-t:.3f}')
